5beda08f2d68ad701c8a929e3f57d7323283cb94
Derek Bourgeois <derek@ibourgeois.com>
2015-04-17 11:06:47 -0400

    Create GuzzleCache.php

---
+++ src/GuzzleCache/Facades/GuzzleCache.php
@@ -0,0 +1,17 @@
+<?php namespace RemiCollin\GuzzleCache\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class Guzzle extends Facade
+{
+
+    protected static function getFacadeAccessor()
+    {
+
+    	$client = \GuzzleHttp\Client;
+
+        return $client;
+        
+    }
+
+}
